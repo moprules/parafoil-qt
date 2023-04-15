8a184dec871b3daf6570d6a016d920676b1e692e
moprules <rav-navini-gego-cutropal@yandex.ru>
2023-04-15 22:15:02 +0300

    parafoil rep integration

---
+++ main.py
@@ -0,0 +1,11 @@
+from parafoil import PFSim
+
+
+def main():
+    lander = PFSim("data/space_rider.yaml")
+    lander.start()
+    print(lander.state["mesh"]["Vinf"])
+
+
+if __name__ == "__main__":
+    main()
