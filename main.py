ffdcc317908497b83bed4260f62294635c34760b
moprules <rav-navini-gego-cutropal@yandex.ru>
2023-04-22 13:13:39 +0300

    grafics test

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import sys
+from PySide6 import QtWidgets
 from parafoil import PFSim
+from flyplot import Graph3DWindow
 
 
 def main():
+    app = QtWidgets.QApplication.instance() or QtWidgets.QApplication(sys.argv)
+    w = Graph3DWindow("grafics/matlab.txt")
+
     lander = PFSim("data/space_rider.yaml")
     lander.start()
-    # print(lander.state["mesh"]["Vinf"])
+    w.graph.addChart("grafics/ans.txt")
+    w.show()
+    sys.exit(app.exec())
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
